4d7cbe24168a711cfc260ff40428b82199ecc9c2
AeonX <kedaren.dorasami@gmail.com>
2019-04-14 14:46:40 +0400

    pass data from nested component to parent

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
 import { NgOnChangesFeature } from '@angular/core/src/render3';
 
 @Component({
@@ -9,10 +9,16 @@ import { NgOnChangesFeature } from '@angular/core/src/render3';
 export class StarComponent implements OnChanges {
     @Input() rating: number;
     starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> = 
+                    new EventEmitter<string>();
     
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
         console.log('starting', this.starWidth);
+    }
+
+    onClick(): void {
+        this.ratingClicked.emit(` This rating ${this.rating} was clicked`);
     }
 
 }
